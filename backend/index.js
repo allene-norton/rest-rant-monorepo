e9c939ec5671ae0d44665c7834c23aca2aed64de
Allene Norton <norton.allene@gmail.com>
2022-03-16 14:28:37 -0500

    9.3.4 working

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 
+
 // Express Settings
+
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/auth_controller'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -12,12 +12,28 @@ function LoginForm() {
         email: '',
         password: ''
     })
+    console.log('Credentials: ', credentials)
 
     const [errorMessage, setErrorMessage] = useState(null)
 
     async function handleSubmit(e) {
         e.preventDefault()
-       
+        const response = await fetch(`http://localhost:5000/authentication/`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+
+        const data = await response.json()
+        
+        if (response.status === 200) {
+            setCurrentUser(data.user)
+            history.push(`/`)
+        } else {
+            setErrorMessage(data.message)
+        }
 
     }
 
